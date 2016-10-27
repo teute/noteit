8c0319ca5f9bd67ffa3b6150de2baa18003ea976
Salieu Kamara <kamaras@ecm15-mac-17.coventry.ac.uk>
2016-10-27 10:02:12 +0100

    initial commit

---
+++ Note It/Note It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Note It
+//
+//  Created by Salieu Kamara on 27/10/2016.
+//  Copyright © 2016 Coventry University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
